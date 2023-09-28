a2d690b72bf993ecbf06863ee0c762c803749038
clay <claycampbell@gmail.com>
2023-09-28 08:16:11 -0700

    new dedup and gold

--- deduplication_tools.py
+++ deduplication_tools.py
@@ -5,12 +5,10 @@ import plotly.express as px
 def deduplication_tools():
     st.title("Deduplication Tools for Stock Data")
 
-   
-     # Sample Data for a financial company
+    # Sample Data for a financial company
     data = {
         'AAPL': ['Apple Inc.', 150, 'Tech'],
         'AAPLE': ['Apple Inc.', 151, 'Tech'],
-        'MSFT': ['Microsoft Corp.', 290, 'Tech'],
         'MSFT': ['Microsoft Corp.', 290, 'Tech'],
         'GOOGL': ['Alphabet Inc.', 2800, 'Tech'],
         'GOOGLE': ['Alphabet Inc.', 2801, 'Tech'],
@@ -20,34 +18,37 @@ def deduplication_tools():
 
     df = pd.DataFrame.from_dict(data, orient='index', columns=['Company Name', 'Current Price', 'Sector'])
     
-    # Step 1: Selection of Dataset
-    # Here, I'm using a single dataset for demonstration. In a real-world application, you would provide options.
     st.write(df)
 
-    # Step 2: Deduplication Criteria
+    # Deduplication Method
+    st.subheader("Deduplication Method")
+    dedup_method = st.radio("Choose deduplication method:", ["Deterministic", "Probabilistic"])
+    
+    # Deduplication Criteria
     st.subheader("Deduplication Criteria")
     columns_to_check = st.multiselect("Select columns to identify duplicates", list(df.columns), default=list(df.columns))
 
-    # Identify duplicates based on the selected columns
-    mask = df.duplicated(subset=columns_to_check, keep=False)
-    if mask.any(): # This is where we ensure we don't evaluate a Series directly in boolean context
+    # Identify duplicates
+    if dedup_method == "Deterministic":
+        mask = df.duplicated(subset=columns_to_check, keep=False)
+    else:
+        st.warning("Probabilistic deduplication is not implemented yet!")
+        return
+
+    if mask.any():
         duplicate_df = df[mask]
-        
-        # Step 3: Display Duplicate Records
         st.subheader("Duplicate Records")
         st.write(duplicate_df)
 
-        # Step 4: Actions on Duplicates
+        # Actions on Duplicates
         st.subheader("Actions")
         if st.button("Merge Duplicates"):
-            # Logic to merge duplicates; for now, a simple message
             st.success("Duplicates Merged!")
 
         if st.button("Delete Duplicates"):
-            # Logic to delete duplicates; for now, a simple message
             st.success("Duplicates Deleted!")
 
-        # Step 5: Visualization
+        # Visualization
         st.subheader("Visualization")
         dup_count = duplicate_df['Company Name'].value_counts()
         fig = px.bar(x=dup_count.index, y=dup_count.values, title="Companies with most duplicates", labels={'x': 'Company Name', 'y': 'Number of duplicates'})
--- golden_record.py
+++ golden_record.py
@@ -1,55 +1,142 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+
 def golden_record():
     st.title("Golden Record Visualization for Stock Data")
 
-    # Sample Data
-    golden_record = {
-        'Ticker Symbol': 'AAPL',
-        'Company Name': 'Apple Inc.',
-        'Current Price': '$150',
-        '52-Week High': '$170',
-        '52-Week Low': '$120',
-        'Market Cap': '$2.4T',
-        'P/E Ratio': '28.5'
-    }
-
-    source_data = {
-        'Source A': {
-            'Ticker Symbol': 'AAPL',
-            'Company Name': 'Apple',
-            'Current Price': '$151',
-            '52-Week High': '$169',
-            '52-Week Low': '$121',
-            'Market Cap': '$2.3T',
-            'P/E Ratio': '28.0'
+    # Golden Record Sample Data
+    golden_record_data = {
+        'AAPL': {
+            'Company Name': 'Apple Inc.',
+            'Current Price': 150,
+            '52-Week High': 170,
+            '52-Week Low': 120,
+            'Market Cap': '2.4T',
+            'P/E Ratio': 28.5
         },
-        'Source B': {
-            'Ticker Symbol': 'AAPL',
-            'Company Name': 'Apple Inc',
-            'Current Price': '$150',
-            '52-Week High': '$171',
-            '52-Week Low': '$119',
-            'Market Cap': '$2.5T',
-            'P/E Ratio': '29.0'
+        'MSFT': {
+            'Company Name': 'Microsoft Corporation',
+            'Current Price': 290,
+            '52-Week High': 310,
+            '52-Week Low': 250,
+            'Market Cap': '2.0T',
+            'P/E Ratio': 32.1
+        },
+        'GOOGL': {
+            'Company Name': 'Alphabet Inc.',
+            'Current Price': 2800,
+            '52-Week High': 2900,
+            '52-Week Low': 2400,
+            'Market Cap': '1.8T',
+            'P/E Ratio': 30.2
+        },
+        'AMZN': {
+            'Company Name': 'Amazon.com Inc.',
+            'Current Price': 3400,
+            '52-Week High': 3500,
+            '52-Week Low': 3200,
+            'Market Cap': '1.7T',
+            'P/E Ratio': 31.0
         }
     }
 
-    # Display Golden Record
-    st.subheader("Golden Record")
-    for key, value in golden_record.items():
+    # Source Data Sample
+    source_data = {
+        'Source A': {
+            'AAPL': {
+                'Company Name': 'Apple',
+                'Current Price': 151,
+                '52-Week High': 169,
+                '52-Week Low': 121,
+                'Market Cap': '2.3T',
+                'P/E Ratio': 28.0
+            },
+            'MSFT': {
+                'Company Name': 'Microsoft Corp.',
+                'Current Price': 289,
+                '52-Week High': 309,
+                '52-Week Low': 251,
+                'Market Cap': '1.9T',
+                'P/E Ratio': 32.0
+            },
+            'GOOGL': {
+                'Company Name': 'Alphabet',
+                'Current Price': 2790,
+                '52-Week High': 2880,
+                '52-Week Low': 2410,
+                'Market Cap': '1.7T',
+                'P/E Ratio': 29.9
+            },
+            'AMZN': {
+                'Company Name': 'Amazon',
+                'Current Price': 3390,
+                '52-Week High': 3490,
+                '52-Week Low': 3190,
+                'Market Cap': '1.6T',
+                'P/E Ratio': 30.5
+            }
+        },
+        'Source B': {
+            'AAPL': {
+                'Company Name': 'Apple Inc',
+                'Current Price': 149,
+                '52-Week High': 171,
+                '52-Week Low': 119,
+                'Market Cap': '2.5T',
+                'P/E Ratio': 29.0
+            },
+            'MSFT': {
+                'Company Name': 'Microsoft',
+                'Current Price': 288,
+                '52-Week High': 307,
+                '52-Week Low': 248,
+                'Market Cap': '1.8T',
+                'P/E Ratio': 31.8
+            },
+            'GOOGL': {
+                'Company Name': 'Alphabet Co.',
+                'Current Price': 2785,
+                '52-Week High': 2875,
+                '52-Week Low': 2395,
+                'Market Cap': '1.6T',
+                'P/E Ratio': 29.8
+            },
+            'AMZN': {
+                'Company Name': 'Amazon Inc',
+                'Current Price': 3385,
+                '52-Week High': 3485,
+                '52-Week Low': 3185,
+                'Market Cap': '1.5T',
+                'P/E Ratio': 30.3
+            }
+        }
+    }
+    st.subheader("Survivorship Rules")
+    preference_order = st.selectbox("Select the source you trust more", list(source_data.keys()), index=0)
+
+    # Let the user select which stock to visualize and compare
+    selected_stock = st.selectbox("Select a stock to visualize", list(golden_record_data.keys()))
+
+    # Update Golden Record based on preference
+    for key in golden_record_data[selected_stock].keys():
+        if golden_record_data[selected_stock][key] != source_data[preference_order][selected_stock][key]:
+            golden_record_data[selected_stock][key] = source_data[preference_order][selected_stock][key]
+
+    # Display Updated Golden Record
+    st.subheader(f"Golden Record for {selected_stock} (Updated based on Survivorship Rules)")
+    for key, value in golden_record_data[selected_stock].items():
         st.write(f"{key}: {value}")
 
-   # Source Contributions
+    # Source Contributions
     st.subheader("Source Contributions")
     source_to_compare = st.selectbox("Select a source to compare", list(source_data.keys()))
     comparison_data = source_data[source_to_compare]
 
-    # Create a bar chart to visualize the differences
-    attributes = list(golden_record.keys())[2:]  # Exclude Ticker and Company Name for visualization
-    golden_values = [golden_record[attr] for attr in attributes]
-    source_values = [comparison_data[attr] for attr in attributes]
+    # Bar chart for visualization
+    attributes = list(golden_record_data[selected_stock].keys())[2:]
+    golden_values = [golden_record_data[selected_stock][attr] for attr in attributes]
+    source_values = [comparison_data[selected_stock][attr] for attr in attributes]
 
     df = pd.DataFrame({
         'Attributes': attributes,
@@ -57,17 +144,13 @@ def golden_record():
         source_to_compare: source_values
     })
 
-    fig = px.bar(df, x='Attributes', y=['Golden Record', source_to_compare], title="Comparison with Golden Record")
+    fig = px.bar(df, x='Attributes', y=['Golden Record', source_to_compare], title=f"Comparison of {selected_stock} with Golden Record")
     st.plotly_chart(fig)
 
-    # Highlight differences in a comparison view
-    st.subheader("Detailed Comparison with Golden Record")
-    for key, value in golden_record.items():
-        if isinstance(value, str):
-            st.write(f"{key}: {value}")
-        elif comparison_data[key] == value:
+    # Detailed Comparison
+    st.subheader(f"Detailed Comparison of {selected_stock} with Golden Record")
+    for key, value in golden_record_data[selected_stock].items():
+        if comparison_data[selected_stock][key] == value:
             st.write(f"{key}: {value}")
         else:
-            st.write(f'{key}: {value} (Source {source_to_compare}: {comparison_data[key]})', unsafe_allow_html=True)
-
-
+            st.write(f'{key}: {value} (Source {source_to_compare}: {comparison_data[selected_stock][key]})', unsafe_allow_html=True)
